f59435665bbed57dddb1dc20f7e16b0a3916bd1d
Vimal Kumar <Vimal2540.vk@gmail.com>
2025-11-04 13:09:02 +0530

    Scheduling Notifications

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, View } from "react-native";
+import * as Notifications from "expo-notifications";
 
 export default function App() {
+  function ScheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first location",
+        body: "This is the body of the notification.",
+        data: {
+          userName: "Max",
+        },
+      },
+      trigger: {
+        seconds: 5,
+      },
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Schedule Notification"
+        onPress={ScheduleNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
